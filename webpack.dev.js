c0f9044096c087d990c6f7f70f91de0135547dee
ivandnd <121280104+ivandnd@users.noreply.github.com>
2023-11-21 13:35:45 +0800

    Modify webpack dev configuration

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,10 +1,24 @@
 const common = require('./webpack.common.js');
 const {merge} = require('webpack-merge');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = merge(common, {
   mode: 'development',
   devtool: 'inline-source-map',
   devServer: {
     static: 'dist'
-  }
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/assets/template/template.html',
+    })
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+    ],
+  },
 });
