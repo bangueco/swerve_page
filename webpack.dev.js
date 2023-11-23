e25fff878475169345b13de8e030333310aafa7e
ivandnd <121280104+ivandnd@users.noreply.github.com>
2023-11-23 15:16:06 +0800

    Update output for images

--- webpack.dev.js
+++ webpack.dev.js
@@ -22,6 +22,9 @@ module.exports = merge(common, {
       {
         test: /\.(png|svg|jpg|jpeg|gif)$/i,
         type: 'asset/resource',
+        generator: {
+          filename: 'images/[name][ext][query]',
+        }
       },
     ],
   },
